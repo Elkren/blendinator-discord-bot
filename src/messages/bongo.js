9d92cfd0333d750365c18fdeeb11dca63c9acf21
Elkren <sam.pallesclark@gmail.com>
2020-09-29 21:27:41 +0100

    Added more jokes

--- src/messages/bongo.js
+++ src/messages/bongo.js
@@ -2,6 +2,16 @@ const setupBongo = (msg) => {
   if (msg.content.toLowerCase().includes("bong")) {
     msg.react(msg.guild.emojis.cache.get("678058088686813184"));
   }
+
+  if (msg.content.includes("anand")) {
+    msg.channel.send("💩");
+  }
+
+  if (msg.content.includes("pog")) {
+    msg.react("🅿");
+    msg.react("🅾");
+    msg.react("🇬");
+  }
 };
 
 module.exports = {
