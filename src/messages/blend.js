af8f53db3ed6d8e9c126cfa3115f50014ce81fe7
Elkren <sam.pallesclark@gmail.com>
2021-08-28 21:44:56 +0100

    Added a check for messages without mentions - refactor

--- src/messages/blend.js
+++ src/messages/blend.js
@@ -1,36 +1,35 @@
 const setupBlend = async (msg, client) => {
-  if (msg.content.includes("`blend")) {
+  if (!msg.content.includes("`blend")) return;
 
-    if(!msg.content.includes("@<!"))
-      msg.channel.send("You need to @ someone to Blend, baka");
-      return;
-    }
-    
-    var userId = msg.content.split("!").pop().split(">")[0];
-    var user = await client.users.fetch(userId);
-
-    const guild = client.guilds.cache.get("696150952025129081");
-    const member = guild.member(user);
-
-    const originalChannelId = member.voice.channel.id;
-
-    var channelIds = [
-      "729013123360751717",
-      "729013283989749841",
-      "827661871381086220",
-      "807188837146165338",
-    ];
-
-    channelIds.forEach((channelId) => {
-      setTimeout(() => {
-        member.voice.setChannel(channelId);
-
-        if (channelId == channelIds[3]) {
-          member.voice.setChannel(originalChannelId);
-        }
-      }, 100);
-    });
+  if (!msg.content.includes("@<!")) {
+    msg.channel.send("You need to @ someone to Blend, baka");
+    return;
   }
+
+  var userId = msg.content.split("!").pop().split(">")[0];
+  var user = await client.users.fetch(userId);
+
+  const guild = client.guilds.cache.get("696150952025129081");
+  const member = guild.member(user);
+
+  const originalChannelId = member.voice.channel.id;
+
+  var channelIds = [
+    "729013123360751717",
+    "729013283989749841",
+    "827661871381086220",
+    "807188837146165338",
+  ];
+
+  channelIds.forEach((channelId) => {
+    setTimeout(() => {
+      member.voice.setChannel(channelId);
+
+      if (channelId == channelIds[3]) {
+        member.voice.setChannel(originalChannelId);
+      }
+    }, 100);
+  });
 };
 
 module.exports = {
